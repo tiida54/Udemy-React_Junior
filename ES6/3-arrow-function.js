e2ff45bf1a79c347da11ee78490bb3c25627515a
BulbCat.David <tiida541108@gmail.com>
2019-04-09 22:49:48 +0800

    ES6 - 3 and 4
    ES6 - 3 and 4

---
+++ ES6/3-arrow-function.js
@@ -0,0 +1,30 @@
+let add = (n1, n2) => (n1 + n2);
+
+let add2 = (n1, n2) => {
+    return n1 + n2;
+};
+
+setTimeout(() => {
+    console.log("setTimeout-1000")
+}, 1000);
+
+//==
+let show = (msg = "default") => {
+    console.log(msg)
+};
+
+show("Hello");
+show();
+
+//==
+let multiply = (n1, n2) => (n1 * n2);
+
+multiply(3, 4);
+
+function combine(first = "Jedi", last = "Wang", name = first + " " + last) {
+    console.log(name)
+};
+
+combine();
+
+console.log("------------")
---
+++ ES6/4-constructor-and-object.js
@@ -0,0 +1,22 @@
+class Car {
+    constructor(color) {
+        this.color = color;
+        this.speed = 0;
+        console.log("constructor", color);
+    }
+    run(speed) {
+        this.speed = speed;
+        console.log(`Running! ${this.color} Car ${this.speed} KM/HR`);
+    }
+    stop() {
+        this.speed = 0;
+        console.log(`${this.color} Car Stopped`);
+    }
+}
+
+let car1 = new Car("blue");
+let car2 = new Car("green");
+car1.run(50);
+car1.stop();
+
+console.log("------------")
