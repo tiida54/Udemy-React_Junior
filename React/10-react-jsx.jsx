409fa101f64801b172f31729369b8e048ea91bbb
BulbCat.David <tiida541108@gmail.com>
2019-04-13 02:44:08 +0800

    Update 10-react-jsx.jsx
    10-react-jsx

--- React/10-react-jsx.jsx
+++ React/10-react-jsx.jsx
@@ -2,11 +2,19 @@ class MyHead extends React.Component {
     render() {
         // console.log(this.props.level);
         // return React.createElement(`h${this.props.level}`, null, "Hello Component")
-        return <h1>Hello JSX</h1>;
+        return <div className="head" style={{ color: this.props.color }}>Hello JSX {this.props.level}</div>;
     }
 }
-
+class MyHeadList extends React.Component {
+    render() {
+        return <div>
+            <MyHead level="1" color="red" />
+            <MyHead level="2" color="green" />
+            <MyHead level="3" color="blue" />
+        </div>;
+    }
+}
 window.addEventListener("load", () => {
-    let myComponent = <MyHead/>;
+    let myComponent = <MyHeadList />;
     ReactDOM.render(myComponent, document.body);
 });
