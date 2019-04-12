5701c9c8dbd9f4ab1f7dccbbd9a28221096a28c1
BulbCat.David <tiida541108@gmail.com>
2019-04-13 02:44:43 +0800

    11-react-event
    11-react-event

---
+++ React/11-react-event.jsx
@@ -0,0 +1,38 @@
+class MyHead extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { color: "red" }
+    }
+    render() {
+        // console.log(this.props.level);
+        // return React.createElement(`h${this.props.level}`, null, "Hello Component")
+        return <div className="head"
+            style={{ color: this.state.color }}
+            onClick={this.clickHandler.bind(this)}
+            onMouseOver={this.onMouseOverHandler.bind(this)}
+            onMouseOut={this.onMouseOutHandler.bind(this)}>Hello JSX {this.props.level}</div>;
+    }
+    clickHandler(e) {
+        // console.log("clickHandler")
+        this.setState({ color: "blue" });
+    }
+    onMouseOverHandler(e) {
+        this.setState({ color: "green" });
+    }
+    onMouseOutHandler(e) {
+        this.setState({ color: "red" });
+    }
+}
+class MyHeadList extends React.Component {
+    render() {
+        return <div>
+            <MyHead level="1" color="red" />
+            <MyHead level="2" color="green" />
+            <MyHead level="3" color="blue" />
+        </div>;
+    }
+}
+window.addEventListener("load", () => {
+    let myComponent = <MyHeadList />;
+    ReactDOM.render(myComponent, document.body);
+});
